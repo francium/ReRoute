9846048ab80a0d5bd3c299e4a83792ec7d446c29
francium <francium@protonmail.com>
2016-03-11 12:51:05 -0500

    made constructor

--- cas2xb3/group40/Camera.java
+++ cas2xb3/group40/Camera.java
@@ -1,4 +1,15 @@
 package cas2xb3.group40;
 
 public class Camera {
+    private int minX;
+    private int minY;
+    private int maxX;
+    private int maxY;
+
+    public Camera(int minX, int minY, int maxX, int maxY) {
+        this.minX = minX;
+        this.minY = minY;
+        this.maxX = maxX;
+        this.maxY = maxY;
+    }
 }
