26fca2a99a6abd98fabcf4e49fff65deddc4d3bf
francium <francium@protonmail.com>
2016-03-12 15:50:44 -0500

    commented out zoom method

--- cas2xb3/group40/Camera.java
+++ cas2xb3/group40/Camera.java
@@ -1,15 +1,31 @@
 package cas2xb3.group40;
 
 public class Camera {
+
     private int minX;
     private int minY;
     private int maxX;
     private int maxY;
 
     public Camera(int minX, int minY, int maxX, int maxY) {
+        if (minX > maxX || minY > maxY) throw new IllegalArgumentException();
         this.minX = minX;
         this.minY = minY;
         this.maxX = maxX;
         this.maxY = maxY;
     }
+
+    public void pan(int dX, int dY) {
+        minX += dX;
+        minY += dY;
+        maxX += dX;
+        maxY += dY;
+    }
+
+    // TODO
+    /* public void zoom(float z) {
+        int dx = ((minX - maxX) / 2);
+        int dx = ((minY - maxY) / 2);
+    } */
+
 }
